4a535c5c28d63e1f97d33a2a4c0a2f75b92761dc
lexrayne <lexrayne@outlook.com>
2023-07-05 19:52:42 -0300

    Prompts for logo options

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const inquirer = require('inquirer');
 
 function init() {
-    console.log("Hello World");
+    inquirer
+        .prompt ([
+            {
+                name: "text",
+                message: "Enter the logo's text (maximum 3 letters):"
+            },
+            {
+                name: "color",
+                message: "Choose the logo's color (by keyword or hexidecimal number):"
+            },
+            {
+                type: "list",
+                name: "shape",
+                message: "Choose the shape of your logo:",
+                choices: ["Circle", "Triangle", "Square"]
+            }
+        ])
 }
 
 init();
