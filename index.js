745b3eeba5a9460b4c1231988d30d5e4abd5bc4d
lexrayne <lexrayne@outlook.com>
2023-07-09 02:52:39 -0300

    Writes user input to SVG file

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const inquirer = require('inquirer');
+const fs = require ('fs');
+const shapes = require("./lib/shapes.js");
+
+function writeToSVG(shape, text) {
+    const data = `<svg version = "1.1"
+        width="300" height="200"
+        xmlns="http://www.w3.org/2000/svg">
+        
+        ${shape.render()}
+        
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">${text}</text>
+        </svg>`;
+    
+    fs.writeFile('shape.svg', data, (err) =>
+        err ? console.error(err) : console.log("SVG created successfully.")
+    );
+}
 
 function init() {
     inquirer
@@ -25,10 +42,16 @@ function init() {
             else {
                 switch (answers.shape) {
                     case "Circle":
+                        const circle = new shapes.Circle(answers.color);
+                        writeToSVG(circle, answers.text);
                         break;
                     case "Triangle":
+                        const triangle = new shapes.Triangle(answers.color);
+                        writeToSVG(triangle, answers.text);
                         break;
                     case "Square":
+                        const square = new shapes.Square(answers.color);
+                        writeToSVG(square, text);
                         break;
                 }
             }
